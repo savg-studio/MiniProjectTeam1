fa30e0e0daef84f12528128de7db06cc572085bf
MartGon <gudo35@gmail.com>
2020-11-02 19:06:08 +0100

    Implemented shield and health system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // UI
+    public ArmorDisplay display;
+
     // Speed
     public float maxSpeed;
     public float minSpeed;
@@ -19,46 +22,71 @@ public class Player : MonoBehaviour
     private bool dashing = false;
     private bool dashInCooldown = false;
 
-    // Shoot
+    // Laser
     public float laserDuration;
     public float laserInterval;
     public bool laserRecoilEnabled;
     public float laserRecoilForce;
     public float laserRecoilDuration;
 
-    // State
+    // Stun effects
     public float stunDuration;
     private bool stunned = false;
 
+    // Invulnerability
     public float invulnerabilityDuration;
     private bool invulnerable = false;
 
+    // Laser
     private bool laserReady = true;
     private bool inRecoil = false;
+
+    // HP
+    public uint maxArmor;
+    private uint currentArmor;
+
+    // Shields
+    public float shieldCooldown;
+    private GameObject shieldGO;
+    private float shieldTimeLeft;
 
     // Components
     private Rigidbody2D rigidBody2D;
     private Animation blinkAnimation;
     private SpriteRenderer spriteRenderer;
 
+    // GameObjects
+    private GameObject sprite;
     private GameObject attack;
-
 
     // Start is called before the first frame update
     void Start()
     {
+        // GameObjects
+        sprite = transform.Find("Sprite").gameObject;
+        attack = transform.Find("Attack").gameObject;
+        shieldGO = transform.Find("Shield").gameObject;
+
+        // Components
         rigidBody2D = GetComponent<Rigidbody2D>();
         blinkAnimation = GetComponentInChildren<Animation>();
-        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        spriteRenderer = sprite.GetComponent<SpriteRenderer>();
 
-        attack = transform.Find("Attack").gameObject;
+        // Main values
+        currentArmor = maxArmor;
+
+        // UI
+        display.SetMaxArmor(maxArmor);
+        display.SetCurrentArmor(maxArmor);
     }
 
     // Update is called once per frame
     void Update() 
     {
+        // Dash
         if (Input.GetKeyDown(KeyCode.Space) && CanStartDash())
             Dash();
+        // Movement speed
         else
         {
             if (Input.GetAxis("Vertical") == 1)
@@ -71,10 +99,19 @@ public class Player : MonoBehaviour
 
         if(!stunned)
         {
-            if(Input.GetButton("Fire1") && laserReady)
+            // Attack
+            if(Input.GetButton("Fire1") && CanLaser())
             {
                 Laser();
             }
+        }
+
+        // Shield
+        if(!IsShieldUp())
+        {
+            UpdateShieldTimer();
+            if (IsShieldReady())
+                EnableShield();
         }
     }
 
@@ -91,49 +128,6 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void Laser()
-    {
-        attack.SetActive(true);
-        laserReady = false;
-        if(laserRecoilEnabled)
-            Recoil();
-
-        Invoke("StopLaser", laserDuration);
-        Invoke("RecoverLaser", laserInterval);
-        Invoke("RecoverFromRecoil", laserRecoilDuration);
-    }
-
-    private void StopLaser()
-    {
-        attack.SetActive(false);
-    }
-    private void RecoverLaser()
-    {
-        laserReady = true;
-    }
-
-    private void Recoil()
-    {
-        inRecoil = true;
-        rigidBody2D.velocity = Vector2.zero;
-        rigidBody2D.AddForce(GetRecoilDir() * laserRecoilForce);
-    }
-    private void RecoverFromRecoil()
-    {
-        inRecoil = false;
-    }
-
-    private Vector2 GetRecoilDir()
-    {
-        var recoilDir =  -1 * GetCurrentDirection();
-        return recoilDir;
-    }
-
-    private bool CanLaser()
-    {
-        return laserReady;
-    }
-
     private Vector3 GetInputRotation()
     {
         Vector3 currentRotation = transform.rotation.eulerAngles;
@@ -141,10 +135,11 @@ public class Player : MonoBehaviour
         return new Vector3(currentRotation.x, currentRotation.y, newZRotation);
     }
 
+    // Movement
     private void Rotate(Vector3 rotation)
     {
         // Updates rigidbody velocity
-        Vector2 direction = AngleToVector2(rotation.z); // If angle is zero, default direction should be (0, 1) instead of (1, 0)
+        Vector2 direction = T1Utils.AngleToVector2(rotation.z); // If angle is zero, default direction should be (0, 1) instead of (1, 0)
         direction.Normalize();
         rigidBody2D.SetRotation(rotation.z);
     }
@@ -155,6 +150,21 @@ public class Player : MonoBehaviour
         Vector2 nextPos = transform.position + dir * speed * Time.deltaTime;
         rigidBody2D.MovePosition(nextPos);
     }
+
+    private void ResetAngularVelocity()
+    {
+        rigidBody2D.angularVelocity = 0;
+    }
+
+    Vector2 GetCurrentDirection()
+    {
+        Vector3 currentRotation = transform.rotation.eulerAngles;
+        Vector2 dir = T1Utils.AngleToVector2(currentRotation.z);
+        dir.Normalize();
+        return dir;
+    }
+
+    // Dash
     private void Dash()
     {
         Vector2 force = GetCurrentDirection() * dashForce * speed;
@@ -164,9 +174,10 @@ public class Player : MonoBehaviour
         Invoke("EnableDash", dashCooldown);
         rigidBody2D.AddForce(force, ForceMode2D.Impulse);
     }
+
     private bool CanStartDash()
     {
-        return !dashing && !dashInCooldown;
+        return !dashing && !dashInCooldown && !inRecoil;
     }
 
     private void StopDashing()
@@ -181,19 +192,65 @@ public class Player : MonoBehaviour
         dashInCooldown = false;
     }
 
+    // Laser
+    private void StopLaser()
+    {
+        attack.SetActive(false);
+    }
+
+    private void RecoverLaser()
+    {
+        laserReady = true;
+    }
+
+    private void Recoil()
+    {
+        inRecoil = true;
+        rigidBody2D.velocity = Vector2.zero;
+        rigidBody2D.AddForce(GetRecoilDir() * laserRecoilForce);
+    }
+
+    private void RecoverFromRecoil()
+    {
+        inRecoil = false;
+    }
+
+    private Vector2 GetRecoilDir()
+    {
+        var recoilDir = -1 * GetCurrentDirection();
+        return recoilDir;
+    }
+
+    private bool CanLaser()
+    {
+        return laserReady;
+    }
+
+    private void Laser()
+    {
+        attack.SetActive(true);
+        laserReady = false;
+        if (laserRecoilEnabled)
+            Recoil();
+
+        Invoke("StopLaser", laserDuration);
+        Invoke("RecoverLaser", laserInterval);
+        Invoke("RecoverFromRecoil", laserRecoilDuration);
+    }
+
+    // Stun
     private void Stun(Projectile p)
     {
         stunned = true;
         Invoke("Recover", stunDuration);
-        StartInvulnerability();
     }
 
     private void Recover()
     {
         stunned = false;
-        ResetAngularVelocity();
     }
 
+    // Invulnerability
     private void StartInvulnerability()
     {
         invulnerable = true;
@@ -201,6 +258,7 @@ public class Player : MonoBehaviour
 
         Invoke("StopInvulnerability", invulnerabilityDuration);
     }
+   
     private void StopInvulnerability()
     {
         invulnerable = false;
@@ -208,29 +266,68 @@ public class Player : MonoBehaviour
         spriteRenderer.enabled = true;
     }
 
-    private void ResetAngularVelocity()
+    // HP
+    private void TakeDamage()
     {
-        rigidBody2D.angularVelocity = 0;
+        if (currentArmor == 0)
+            Die();
+        else
+            currentArmor--;
+
+        display.SetCurrentArmor(currentArmor);
     }
 
-    Vector2 GetCurrentDirection()
+    private bool IsShieldUp()
     {
-        Vector3 currentRotation = transform.rotation.eulerAngles;
-        Vector2 dir = AngleToVector2(currentRotation.z);
-        dir.Normalize();
-        return dir;
+        return shieldGO.activeSelf;
     }
 
-    Vector2 AngleToVector2(float angle)
+    private void DisableShield()
     {
-        return new Vector2(Mathf.Cos(Mathf.Deg2Rad * angle), Mathf.Sin(Mathf.Deg2Rad * angle));
+        shieldGO.SetActive(false);
     }
 
+    private void EnableShield()
+    {
+        shieldGO.SetActive(true);
+    }
+
+    private void ResetShieldTimer()
+    {
+        shieldTimeLeft = shieldCooldown;
+    }
+    private bool IsShieldReady()
+    {
+        return shieldTimeLeft < 0;
+    }
+
+    private void UpdateShieldTimer()
+    {
+        shieldTimeLeft -= Time.deltaTime;
+    }
+
+    private void Die()
+    {
+
+    }
+
+    // Collision
     public void CollideWith(Projectile p, Collision2D collision)
     {
-        if (!invulnerable)
+        if (IsShieldUp())
+        {
+            DisableShield();
+            StartInvulnerability();
+
+            ResetShieldTimer();
+        }
+        else if (!invulnerable)
         {
             Stun(p);
+            TakeDamage();
+            StartInvulnerability();
+
+            ResetShieldTimer();
         }
     }
 }
---
+++ Assets/Scripts/UI/ArmorDisplay.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArmorDisplay : MonoBehaviour
+{
+    // Cache
+    private Text text;
+
+    // Value
+    private uint maxArmor;
+    private uint currentArmor;
+    private string textFormat = "Armor: {0}/{1}";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetCurrentArmor(uint amount)
+    {
+        currentArmor = amount;
+        UpdateText();
+    }
+
+    public void SetMaxArmor(uint amount)
+    {
+        maxArmor = amount;
+        UpdateText();
+    }
+
+    public void UpdateText()
+    {
+        text.text = string.Format(textFormat, currentArmor, maxArmor);
+    }
+}
