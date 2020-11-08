2319ff49e95c42d5c928682839116d3c68d268d5
MartGon <gudo35@gmail.com>
2020-11-08 17:14:30 +0100

    Implemented Laser

---
+++ Assets/Scripts/Weapons/Laser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public Spaceship owner;
+
+    public void SetOwner(Spaceship owner)
+    {
+        this.owner = owner;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Spaceship ss = collision.gameObject.GetComponent<Spaceship>();
+
+        if (ss && owner != ss)
+            ss.TakeDamage();
+    }
+}
