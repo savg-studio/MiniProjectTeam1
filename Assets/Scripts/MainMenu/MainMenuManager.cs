91d82795ffd2b371d16cf2d367cb1bb0897fbf41
MartGon <gudo35@gmail.com>
2020-11-10 17:38:19 +0100

    Implements MemeMenu

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void OnNewGameClick(AudioSource source)
+    {
+        source.Play();
+        var length = source.clip.length;
+        Invoke("EnterMainGame", length);
+    }
+
+    public void OnCloseGameClick()
+    {
+        Application.Quit();
+    }
+
+    public void EnterMainGame()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+}
